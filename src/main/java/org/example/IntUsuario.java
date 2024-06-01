bd57b0d9e7478f72d8df561ace3c99cfbba249a1
XDeme <fernando.tagawa.gamail.com@gmail.com>
2024-06-01 15:38:15 -0300

    Fix

--- src/main/java/org/example/IntUsuario.java
+++ src/main/java/org/example/IntUsuario.java
@@ -57,7 +57,7 @@ public class IntUsuario {
         System.out.println("Selecione uma opção:\n"
                 + "-1 -> Sair\n"
                 + "0 -> Criar equipe\n"
-                + "id -> Entrar ID player\n");
+                + "id -> Entrar ID Equipe\n");
         int option = ler.nextInt();
         if(option == -1)
             return null;
@@ -71,7 +71,41 @@ public class IntUsuario {
             System.out.println();
             return CriarEquipe(nome_usuario, cod);
         }
+
+        ResultSet res = Main.db.consulta("SELECT e.id, e.nome, p.id, p.nickname, p.nivel, p.exp, p.id_classe " +
+                "FROM Equipe AS e " +
+                "INNER JOIN Player AS p ON e.id = p.id_equipe " +
+                "WHERE e.id = " + option + " AND id_usuario_humano IS NOT NULL;");
+        res.next();
+        int id = res.getInt("e.id");
+        String nome_equipe = res.getString("e.nome");
+        ArrayList<Player> players = new ArrayList<>();
+        for(int i = 0; i < 3; ++i) {
+            int p_id = res.getInt("p.id");
+            String p_name = res.getString("p.nickname");
+            int id_classe = res.getInt("p.id_classe");
+            players.add(switch (id_classe) {
+                case 1: {
+                    Player p = new Player(p_name, new Warrior());
+                    p.set_id(p_id);
+                    yield p;
+                }
+                case 2: {
+                    Player p = new Player(p_name, new Archer());
+                    p.set_id(p_id);
+                    yield p;
+                }
+                case 3: {
+                    Player p = new Player(p_name, new Mage());
+                    p.set_id(p_id);
+                    yield p;
+                }
+                default: yield null;
+            });
+            res.next();
+        }
+        Equipe e = new Equipe(players, nome_equipe, id);
         // Entrar ID Player
-        return null;
+        return e;
     }
 }
